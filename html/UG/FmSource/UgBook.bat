57fcce40885eaf062061312660cb8f2068958fff
Mark Evans <evan@hdfgroup.org>
2015-07-17 14:06:29 -0500

    FrameMaker source files for the HDF5 User's Guide.
    Former-commit-id: cb8627e1346493f1bebb0e380df07d13bf48214f

---
+++ html/UG/FmSource/UgBook.bat
@@ -0,0 +1,10 @@
+copy 08_TheFile.html_150210.fm 03_DataModel.html_150210.fm
+copy 08_TheFile.html_150210.fm 04_ProgModel.html_150210.fm
+copy 08_TheFile.html_150210.fm 09_Groups.html_150210.fm
+copy 08_TheFile.html_150210.fm 10_Datasets.html_150210.fm
+copy 08_TheFile.html_150210.fm 11_Datatypes.html_150210.fm
+copy 08_TheFile.html_150210.fm 12_Dataspaces.html_150210.fm
+copy 08_TheFile.html_150210.fm 13_Attributes.html_150210.fm
+copy 08_TheFile.html_150210.fm 13_ErrorHandling.html_150210.fm
+copy 08_TheFile.html_150210.fm 14_PropertyLists.html_150210.fm
+copy 08_TheFile.html_150210.fm 17_Additional.html_150210.fm
