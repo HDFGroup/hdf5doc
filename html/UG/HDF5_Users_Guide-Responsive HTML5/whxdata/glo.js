d2096e1a3354c590cdb632258fa0abf8a79c6cbf
Frank Baker <fbaker@hdfgroup.org>
2016-05-19 16:04:05 -0500

    Create hdf5_1_10_0 release branch.

---
+++ html/UG/HDF5_Users_Guide-Responsive HTML5/whxdata/glo.js
@@ -0,0 +1,1 @@
+gXMLBuffer ="<?xml version=\"1.0\" encoding=\"utf-8\" ?><glossary>  <chunkinfo url=\"gdata1.js\" first=\"attribute\" last=\"hard link\" num=\"26\" />  <chunkinfo url=\"gdata2.js\" first=\"hyperslab\" last=\"symbolic link\" num=\"21\" />  </glossary>";
