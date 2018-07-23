1a5fcf463c2b8977ed7b20a4b8fcfc4d9cf56e4e
bljones <bljones@hdfgroup.org>
2018-07-23 08:24:45 -0500

    added new C++ doc with corrected table link on first page
    Former-commit-id: 7a8e0f18243d29be8d5f3d511daa72b49cc8bdb5

--- html/cpplus_RM/search/typedefs_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['attr_5foperator_5ft',['attr_operator_t',['../namespace_h5.html#aaa21225c769608fd0660459661b1ef28',1,'H5']]]
-];
--- html/cpplus_RM/search/variables_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['fortran_5fs1',['FORTRAN_S1',['../class_h5_1_1_pred_type.html#a2d89593e55cb0b0df668902c2d02191e',1,'H5::PredType']]]
-];
