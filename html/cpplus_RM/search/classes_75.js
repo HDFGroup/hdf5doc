59ca1178772282d8d520fca359154150b74bc625
bljones <bljones@hdfgroup.org>
2020-04-08 12:47:25 -0500

    old version of C++
    Former-commit-id: 8530924bae7452411234d45c769dc104b956fc5e

--- html/cpplus_RM/search/classes_75.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['userdata4aiterate',['UserData4Aiterate',['../class_h5_1_1_user_data4_aiterate.html',1,'H5']]],
-  ['userdata4visit',['UserData4Visit',['../class_h5_1_1_user_data4_visit.html',1,'H5']]]
-];
--- html/cpplus_RM/search/typedefs_76.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['visit_5foperator_5ft',['visit_operator_t',['../namespace_h5.html#a78a19eb0783fd7c17898a6c8a4eb5b8d',1,'H5']]]
-];
